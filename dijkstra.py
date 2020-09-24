0a3644f1abb2879d819e1ff9ba3f4d4488647eb5
endhel <endheltog@gmail.com>
2020-09-24 13:34:45 -0300

    Finished dijkstra algorithm

--- dijkstra.py
+++ dijkstra.py
@@ -1,19 +1,38 @@
 def dijkstra(g, origin):
-    dist = []
-    pred = []
+    distance = []
+    predecessor = []
 
     for i in range(len(g)):
-        dist.append(float('inf'))
-        pred.append(None)
-    dist[origin] = 0
-    Q = g
+        distance.append(float('inf'))
+        predecessor.append(None)
+    distance[origin] = 0
+    vertices = [i for i in range(len(g))]
+
+    while len(vertices) != 0:
+        minimum = float('inf')
+        u = -1
+        for i in vertices:
+            if distance[i] < minimum:
+                minimum = distance[i]
+                u = i
+        adjacency = g[u]
+        vertices.remove(u)
+        index = 0
+
+        for v in adjacency[0]:
+            if distance[v] > distance[u] + adjacency[1][index]:
+                distance[v] = distance[u] + adjacency[1][index]
+                predecessor[v] = u
+            index += 1
+
+    print(distance)
+    print(predecessor)
 
 
-graph = [[(1, 2), (2, 6)],
-         [(2, 5), (4, 3)],
-         [(1, 5), (3, 4)],
-         [(0, 2), (4, 7)],
-         [(0, 1), (3, 7)],
-         []]
+graph = [[(1, 2, 3), (3, 1, 1)],
+         [(2, 4), (4, 1)],
+         [(0, 1, 3, 4), (1, 4, 5, 5)],
+         [(0,), (1,)],
+         [(0, 2, 3), (4, 5, 2)]]
 
-dijkstra(graph, 0)
+dijkstra(graph, 2)
